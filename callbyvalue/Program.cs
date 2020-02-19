44d211d82d74b2378dda95a59cdb9b57d5f14805
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:30:44 +0530

    Initial commit

---
+++ callbyvalue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callbyvalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+           
+            a = 10;
+            b = 20;
+            ShowData(a, b);
+        }
+        static void ShowData(int m,int n)
+        {
+            
+            double sum = m + n;
+            Console.WriteLine("sum={0}", sum);
+            m = m + 1;
+            n = n + 1;
+        }
+    }
+}
